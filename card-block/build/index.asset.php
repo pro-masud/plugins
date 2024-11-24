20c1323af5490242c73237e75ac9ff62cc61f287
pro-masud <promasudbd@gmail.com>
2024-11-24 08:38:12 +0600

    feat: card-block plugin supports for align apply

--- card-block/build/index.asset.php
+++ card-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '01a1580da5216f85f607');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8aba809078bc76837f37');
--- card-block/build/new-block/index.asset.php
+++ card-block/build/new-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '10882ed1744186b092aa');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '284389c17c0c683facf2');
