5ebc6c300a073d36ecdb5582c8788b6679814c87
pro-masud <promasudbd@gmail.com>
2024-11-24 09:07:24 +0600

    feat: card-block plugin new-block supports remove customclassname option in advance options

--- card-block/build/index.asset.php
+++ card-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '6ecd8ed530950ee33854');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '14fd0f4442895c49d457');
--- card-block/build/new-block/index.asset.php
+++ card-block/build/new-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c2fcacc7218106c49cbb');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c7fa8c438f9e839a5c57');
